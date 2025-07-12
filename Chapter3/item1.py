b402de295d2e8dc500155d9e4e0f5ea7e183757e
Nonnnchern <67011509@kmitl.ac.th>
2025-07-13 01:33:59 +0700

    chapter 3 item 2 done

--- Chapter3/item1.py
+++ Chapter3/item1.py
@@ -6,20 +6,18 @@ class Stack():
    def size_of_stack(self):
       return f"{self.size}"
 
-   def add_into_stack(self, add_function, value):
-      if add_function == 'A':
-         self.stack.append(value)
-         self.size += 1
+   def add_into_stack(self, value):
+      self.stack.append(value)
+      self.size += 1
       print(f"Add = {value} and Size = {self.size}")
 
-   def pop_stack(self, add_function):
-      if add_function == 'P':
-         if not self.stack:
-            print("-1")
-         else:
-            print(f"Pop = {self.stack[-1]} and Index = {self.size - 1}")
-            self.stack.pop()
-            self.size -= 1
+   def pop_stack(self):
+      if not self.stack:
+         print("-1")
+      else:
+         print(f"Pop = {self.stack[-1]} and Index = {self.size - 1}")
+         self.stack.pop()
+         self.size -= 1
 
    def __str__(self):
       output = ''
@@ -36,9 +34,9 @@ a = input("Enter Input : ").split(",")
 for i in range(0,len(a)):
    if len(a[i]) > 1 :
       x = a[i].split()
-      stack.add_into_stack(add_function = x[0],value = x[1])
+      stack.add_into_stack(value = x[1])
    else:
-      stack.pop_stack(add_function = a[i])
+      stack.pop_stack()
 
 
 print(stack)
--- Chapter3/item2.py
+++ Chapter3/item2.py
@@ -1,27 +1,82 @@
 class Stack():
-   pass
+   def __init__(self):
+      self.stack = []
+
+   def add_into_stack(self, value):
+      self.stack.append(value)
+      print(f"Add = {value}")
+
+   def pop_stack(self):
+      if not self.stack:
+         print("-1")
+      else:
+         print(f"Pop = {self.stack[-1]}")
+         self.stack.pop()
+
+   def delete_index(self, value):
+      if not self.stack:
+         print("-1")
+      else:
+         while self.stack:
+            for _ in range(0,len(self.stack)):
+               for i in self.stack:
+                  if i == value:
+                     print(f"Delete = {i}")
+                     self.stack.remove(i)
+            break
+
+   def lessthan_del(self, lessthan_value):
+      temp = []
+      if not self.stack:
+         print("-1")
+      else:
+         while self.stack:
+            for i in self.stack:
+               if i < lessthan_value:
+                  temp.append(i)
+            break
+         temp.sort()
+         for i in temp:
+            for j in self.stack:
+               if j == i:
+                  print(f"Delete = {j} Because {j} is less than {lessthan_value}")
+                  self.stack.remove(j)
+
+   def morethan_del(self, morethan_value):
+      if not self.stack:
+         print("-1")
+      else:
+         while self.stack:
+            
+            for _ in range(0,len(self.stack)):
+               for i in self.stack:
+                  if i > morethan_value:
+                     self.stack.remove(i)
+                     print(f"Delete = {i} Because {i} is more than {morethan_value}")
+            break
+
+   def __str__(self):
+      output = []
+      for i in self.stack:
+         output.append(int(i))
+      return f"Value in Stack = {output}"
 
 stack = Stack()
 a = input("Enter Input : ").split(",")
-print(a)
 for i in range(0,len(a)):
    if len(a[i]) > 1 :
       function, value = a[i].split()
+      value = int(value)
       if function == 'A':
-         pass
-      elif function == 'P':
-         pass
+         stack.add_into_stack(value = value)
       else:
          if function == 'LD' :
-            pass
+            stack.lessthan_del(lessthan_value = value)
          elif function == 'MD' :
-            pass
+            stack.morethan_del(morethan_value = value)
          else:
-            pass
-      print(function)
-      print(value)
-      # stack.add_into_stack(add_function = x[0],value = x[1])
+            stack.delete_index(value = value)
    else:
-      print(a[i])
-      # stack.pop_stack(add_function = a[i])
-      pass
+      stack.pop_stack()
+
+print(stack)
